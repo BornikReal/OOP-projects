3b6e162bfdac44441b3f5a5f6067eef4911fe73b
BornikReal <cool.nikolay@gmail.com>
2022-09-18 18:07:07 +0300

    fear: saving ID in json format

--- Lab0/Isu/Models/GeneratorId.cs
+++ Lab0/Isu/Models/GeneratorId.cs
@@ -7,35 +7,37 @@ public class GeneratorId
     public const int MinId = 100000;
     public const int MaxId = 999999;
 
-    private static readonly string Path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\UserData.txt";
-    public static List<int> GetIdList()
+    private static readonly string Path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\UserData.json";
+    public static List<int>? GetIdList()
     {
-        if (!File.Exists(Path))
-            File.Create(Path).Close();
-        StreamReader sr;
-        sr = new StreamReader(Path);
-
-        var newList = new List<int>();
-        while (!sr.EndOfStream)
+        try
         {
-            _ = int.TryParse(sr.ReadLine(), out int x);
-            newList.Add(x);
+            string fs = File.ReadAllText(Path);
+            return JsonSerializer.Deserialize<List<int>>(fs);
         }
-
-        JsonSerializer.Serialize(newList);
-
-        sr.Close();
-        return newList;
+        catch (FileNotFoundException)
+        {
+            return null;
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
     }
 
     public static bool CheckAvail(int id)
     {
-        return GetIdList().Find(i => i == id) != default;
+        List<int>? list = GetIdList();
+        if (list == null)
+            return false;
+        return list.Find(i => i == id) != default;
     }
 
     public static int Generate()
     {
-        List<int> list = GetIdList();
+        List<int>? list = GetIdList();
+        list ??= new List<int>();
+
         int newId;
         if (list.Count != 0)
             newId = list.Last() + 1;
@@ -51,9 +53,11 @@ public class GeneratorId
                 throw new UnavailableIdException();
         }
 
-        StreamWriter sr = File.AppendText(Path);
-        sr.WriteLine(newId);
-        sr.Close();
+        list.Add(newId);
+
+        var fs = new StreamWriter(Path);
+        fs.Write(JsonSerializer.Serialize<List<int>>(list));
+        fs.Close();
         return newId;
     }
 }
---
+++ TestingApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Isu.Models;
+
+GeneratorId.GetIdList();
+GeneratorId.Generate();
+//using System.Text.Json;
+
+//string Path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\UserData.json";
+
+//var t = new List<int>
+//{
+//    1,
+//    2
+//};
+//string personJson = JsonSerializer.Serialize(t, typeof(List<int>));
+//StreamWriter file = File.CreateText(Path);
+//file.WriteLine(personJson);
+//file.Close();
