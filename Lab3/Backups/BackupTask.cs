ddc2a7ca78eeae5b56ddb23401439d3a7904e460
BornikReal <cool.nikolay@gmail.com>
2022-11-05 00:01:46 +0300

    feat: make BackupTask

--- Lab3/Backups/BackupTask.cs
+++ Lab3/Backups/BackupTask.cs
@@ -1,18 +1,25 @@
-﻿namespace Backups;
+﻿using Backups.Algorithms;
+using Backups.Repository;
+
+namespace Backups;
 
 public class BackupTask
 {
     private readonly List<BackupObject> _backupObjects = new List<BackupObject>();
 
-    public BackupTask(string name)
+    public BackupTask(string name, IAlgorithm algorithm, IArchivator archivator)
     {
         Name = name;
+        Algorithm = algorithm;
+        Archivator = archivator;
     }
 
     public string Name { get; }
     public Backup Backup { get; } = new Backup();
     public IReadOnlyList<BackupObject> BackupObjects => _backupObjects;
     public Guid Id { get; } = Guid.NewGuid();
+    public IAlgorithm Algorithm { get; }
+    public IArchivator Archivator { get; }
 
     public void AddNewTask(BackupObject backupObject)
     {
@@ -26,5 +33,11 @@ public class BackupTask
         if (_backupObjects.Find(s => s == backupObject) == null)
             throw new Exception();
         _backupObjects.Remove(backupObject);
+    }
+
+    public void CreateRestorePoint()
+    {
+        string restorPointPath = "RestorePoint-" + Guid.NewGuid();
+        Backup.AddRestorePoint(new RestorePoint(new List<BackupObject>(_backupObjects), Algorithm.CreateBackup(_backupObjects, Archivator, restorPointPath), DateTime.Now));
     }
 }
