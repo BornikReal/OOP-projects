ac311754b06f32af8e47ea1bc6fec57288e6a5de
BornikReal <cool.nikolay@gmail.com>
2022-11-05 19:23:10 +0300

    feat: add zip file and directory

---
+++ Lab3/Backups/ZipObjects/ZipDirectory.cs
@@ -0,0 +1,12 @@
+﻿using System.IO.Compression;
+using Backups.FileSystemEntities;
+
+namespace Backups.ZipObjects;
+
+public class ZipDirectory : IZipObject
+{
+    public IFileSystemEntity Create(ZipArchiveEntry zipArchiveEntry)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Lab3/Backups/ZipObjects/ZipFile.cs
@@ -0,0 +1,12 @@
+﻿using System.IO.Compression;
+using Backups.FileSystemEntities;
+
+namespace Backups.ZipObjects;
+
+public class ZipFile : IZipObject
+{
+    public IFileSystemEntity Create(ZipArchiveEntry zipArchiveEntry)
+    {
+        throw new NotImplementedException();
+    }
+}
