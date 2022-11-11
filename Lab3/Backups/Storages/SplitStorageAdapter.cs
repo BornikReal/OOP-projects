2ebb181f889c5746f214f2e8b2fde7690917b82a
BornikReal <cool.nikolay@gmail.com>
2022-11-11 09:19:28 +0300

    fix: small fix

--- Lab3/Backups/Storages/SplitStorageAdapter.cs
+++ Lab3/Backups/Storages/SplitStorageAdapter.cs
@@ -4,17 +4,17 @@ namespace Backups.Storages;
 
 public class SplitStorageAdapter : IStorage
 {
-    private readonly SplitStorage _splitStorage;
+    private readonly SplitStorage _adaptee;
 
-    public SplitStorageAdapter(SplitStorage splitStorage)
+    public SplitStorageAdapter(SplitStorage adaptee)
     {
-        _splitStorage = splitStorage;
+        _adaptee = adaptee;
     }
 
     public IEnumerable<IFileSystemEntity> GetEntities()
     {
         var entities = new List<IFileSystemEntity>();
-        foreach (ZipStorage zipStorage in _splitStorage.Storages)
+        foreach (ZipStorage zipStorage in _adaptee.Storages)
             entities.AddRange(zipStorage.GetEntities());
         return entities;
     }
