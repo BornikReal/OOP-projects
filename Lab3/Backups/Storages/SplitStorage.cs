0480307c3f05cb81deabe9a0cec95b575c0f1606
BornikReal <cool.nikolay@gmail.com>
2022-11-11 09:18:46 +0300

    feat: make splitstorage and splitstorageadapter

---
+++ Lab3/Backups/Storages/SplitStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace Backups.Storages;
+
+public class SplitStorage
+{
+    public SplitStorage(IEnumerable<ZipStorage> storages)
+    {
+        Storages = storages;
+    }
+
+    public IEnumerable<ZipStorage> Storages { get; }
+}
---
+++ Lab3/Backups/Storages/SplitStorageAdapter.cs
@@ -0,0 +1,21 @@
+﻿using Backups.FileSystemEntities.Interfaces;
+
+namespace Backups.Storages;
+
+public class SplitStorageAdapter : IStorage
+{
+    private readonly SplitStorage _splitStorage;
+
+    public SplitStorageAdapter(SplitStorage splitStorage)
+    {
+        _splitStorage = splitStorage;
+    }
+
+    public IEnumerable<IFileSystemEntity> GetEntities()
+    {
+        var entities = new List<IFileSystemEntity>();
+        foreach (ZipStorage zipStorage in _splitStorage.Storages)
+            entities.AddRange(zipStorage.GetEntities());
+        return entities;
+    }
+}
